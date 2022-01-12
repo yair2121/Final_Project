d486075cd058d3426cb9dffc74ec2929979e6f2b
yair2121 <77067231+yair2121@users.noreply.github.com>
2022-01-12 13:46:10 +0200

    BaseGameModel(server): Added implementation

--- Backend/Games/BaseGameModel.js
+++ Backend/Games/BaseGameModel.js
@@ -9,7 +9,11 @@ class BaseGameModel {
     this.game_name = game_name;
     this.max_player_count = max_player_count;
     this.min_player_count = min_player_count;
+    this.moves = [];
+    this.isRunning = false;
+    this.start_date = null;
   }
+
   /**
    *
    * @param {number} number_of_players- Fixed number of players in game.
@@ -22,25 +26,72 @@ class BaseGameModel {
     ) {
       throw "Incorrect number of players";
     }
+    this.isRunning = true;
+    this.start_date = new Date();
+    this.moves.push({ Time: this.start_date });
   }
+
   /**
-   * Apply given move to the game
+   * Will validate that given move abide to the game rules and current state.
+   * @param {dictionary} move_description
+   */
+  validate_move(move_description) {
+    interface_default();
+  }
+
+  /**
+   * Will apply given move to the current state of the game.
+   * @param {dictionary} move_description
+   */
+  apply_move(move_description) {
+    interface_default();
+  }
+
+  /**
+   * Will process the given move to fit current game.
+   * @param {dictionary} move_description
+   */
+  process_move(move_description) {
+    interface_default();
+  }
+
+  /**
+   * Will log the given move
+   * @param {dictionary} move_description
+   */
+  log_move(move_description) {
+    this.moves.push(Object.assign({}, { Time: new Date() }, move_description));
+  }
+
+  /**
+   * Will apply given move to the game.
    * @param {dictionary} move_description- Dictionary that hold description of current move,
    */
   make_move(move_description) {
-    interface_default();
+    this.process_move(move_description);
+    this.validate_move(move_description);
+    this.apply_move(move_description);
+    this.log_move(move_description);
   }
   /**
    * @returns Dictionary that hold current state of the game.
    */
   get_state() {
-    interface_default();
+    return { is_running: this.isRunning };
   }
+
+  /**
+   * @returns Latest played move.
+   */
+  get_move() {
+    return this.moves.slice(-1)[0];
+  }
+
   /**
    * @returns Dictionary that hold all the moves that have been made till now.
    */
   get_game_report() {
-    interface_default();
+    return Object.assign({}, { moves: this.moves }, this.get_state());
   }
 }
 
