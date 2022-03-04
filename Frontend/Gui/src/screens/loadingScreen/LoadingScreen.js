4a9a59f0dfb261c1754b5a748318976982fba72b
yair <yair.yardeni@watchful.ai>
2022-03-04 15:59:43 +0200

    GUI(Frontend): Implemented SumGame GUI.

---
+++ Frontend/Gui/src/screens/loadingScreen/LoadingScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+import React from "react";
+import { ActivityIndicator } from "react-native";
+export default function LoadingScreen({ gameName }) {
+  return (
+    <View>
+      <Text>Loading {gameName}</Text>
+      <ActivityIndicator size="large" color="#00ff00" />
+    </View>
+  );
+}
