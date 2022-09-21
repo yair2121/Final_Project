fd1e82b467e8772698499b226ec3dc8c5781ceb5
yair <yair.yardeni@watchful.ai>
2022-09-21 22:07:53 +0300

    Backend-removed unnecessary views files

--- Backend/Games/SumGame/100SumView.js
+++
@@ -1,7 +0,0 @@
-const { BaseSessionView } = require("../BaseSessionView");
-class HundredSumView extends BaseSessionView {
-  constructor() {
-    super();
-  }
-}
-module.exports = { HundredSumView };
