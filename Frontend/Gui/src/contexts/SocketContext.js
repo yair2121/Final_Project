20858f5241c77cf7f62669b91cc1dd6332bf6b03
yair <yair.yardeni@watchful.ai>
2022-09-28 11:39:53 +0300

    SOCKET_URL- changed to localhost

--- Frontend/Gui/src/contexts/SocketContext.js
+++ Frontend/Gui/src/contexts/SocketContext.js
@@ -1,7 +1,7 @@
 const { io } = require("socket.io-client");
 import { createContext } from "react";
 const PORT = "3000"; //port
-const SOCKET_URL = "http://192.168.0.185:" + PORT; //url where server is hosted
+const SOCKET_URL = "http://localhost:" + PORT; //url where server is hosted
 export const socket = io(SOCKET_URL);
 socket.connect();
 export const SocketContext = createContext(null);
